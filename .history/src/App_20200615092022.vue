c54cda1dee353f8630b9b0f4d3da596ff6269354
syawal2kalii <syawal20162016@gmail.com>
2020-06-15 09:32:59 +0800

    5_tambah_router

--- .history/src/App_20200615092022.vue
+++ .history/src/App_20200615092022.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/profile">Profile</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/profil">Profile</router-link>
     </div>
     <router-view/>
   </div>
